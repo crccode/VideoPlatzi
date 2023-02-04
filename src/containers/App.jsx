66a467f80ddd87106910b5fd477d06f4d7212684
crccode <54750603+crccode@users.noreply.github.com>
2023-02-04 07:36:33 -0400

    Creando scss para los componentes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Header from '../components/*Header'
+//ESTILO
+import '../assets/styles/App.scss';
+
 // EN AQUI COLOCAREMOS TODOS LOS ELEMNTOS O COMPONENTES QUE VAMOS A USAR 
 
 
