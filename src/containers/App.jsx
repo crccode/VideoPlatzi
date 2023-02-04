592580f78a4af6bfcd83aa6b89d10ba210c3102a
crccode <54750603+crccode@users.noreply.github.com>
2023-02-04 07:47:10 -0400

    Componente Search

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+// COMPONENTE HEADER
 import Header from '../components/*Header'
+// COMPONENTE SEARCH
+import Search from '../components/Search'
 //ESTILO
 import '../assets/styles/App.scss';
 
 // EN AQUI COLOCAREMOS TODOS LOS ELEMNTOS O COMPONENTES QUE VAMOS A USAR 
-
-
 //ESTA CONSTANTE TENDREMOS NUESTRO COMPONENTES 
 const App= () => {
     <div className='App'>
         <Header/>
+        <Search/>
     </div>
 }; 
 export default App;
